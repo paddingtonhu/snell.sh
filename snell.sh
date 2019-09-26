d4d12b0e474ebb4028e2f295e0ed9912186a8cdc
Gong Cheng <scomper@gmail.com>
2019-09-26 16:48:03 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
-yum install unzip -y
+apt-get install unzip -y
 cd ~/
 wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/v1.1.1/snell-server-v1.1.1-linux-amd64.zip
 unzip -o snell.zip
